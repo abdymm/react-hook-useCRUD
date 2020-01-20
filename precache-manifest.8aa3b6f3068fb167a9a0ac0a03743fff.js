1239debdefa9469d1cdc010dda731ed03095b860
abdymalikmulky <abdymalikmulky@gmail.com>
2020-01-20 07:17:45 +0700

    Updates

---
+++ precache-manifest.8aa3b6f3068fb167a9a0ac0a03743fff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3852f34c0db09c83ca12446739e6f2",
+    "url": "/react-hook-useCRUD/index.html"
+  },
+  {
+    "revision": "b95f518a7736f47a0de0",
+    "url": "/react-hook-useCRUD/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "15f56121699b6edfa19b",
+    "url": "/react-hook-useCRUD/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b95f518a7736f47a0de0",
+    "url": "/react-hook-useCRUD/static/js/2.19a5bf3b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-hook-useCRUD/static/js/2.19a5bf3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15f56121699b6edfa19b",
+    "url": "/react-hook-useCRUD/static/js/main.7d4762c7.chunk.js"
+  },
+  {
+    "revision": "a464f5368d8a87b18677",
+    "url": "/react-hook-useCRUD/static/js/runtime-main.9ddd6476.js"
+  }
+]);
